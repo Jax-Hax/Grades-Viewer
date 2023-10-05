f9a0bd540dfada536b4760d6166dbf2bd7dad0a6
Jax-Hax <jaxhacks543@gmail.com>
2023-10-05 17:12:10 -0400

    added init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    const StudentVue = require('studentvue.js');
+StudentVue.login('district url', 'username', 'password')
+    .then(client => client.getMessages())
+    .then(console.log);
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
