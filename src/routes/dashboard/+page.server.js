7357bb18abb7d6ea1a731883e1aa2c53b7da5d26
Jax-Hax <jaxhacks543@gmail.com>
2023-10-10 17:31:08 -0400

    trying to add a select screen

--- src/routes/dashboard/+page.server.js
+++ src/routes/dashboard/+page.server.js
@@ -1,23 +1,5 @@
-import { login, getDistrictUrls } from '$lib/index';
+import { login } from '$lib/index';
 import { redirect } from '@sveltejs/kit';
-
-/** @type {import('./$types').Actions} */
-export const actions = {
-	default: async ({request, cookies }) => {
-        const data = await request.formData();
-        const url = data.get('schoolURL')
-        const username = data.get('username')
-        const password = data.get('password')
-        cookies.set('url', url, { path: '/' })
-		let client = await login(url, username, password);
-        let grades = await client.getGradebook();
-        getDistrictUrls('23113').then(console.log)
-        return {
-            grades: JSON.parse(grades)
-        };
-	}
-};
-
 
 export async function load({ cookies}) {
     const url = cookies.get("url");
@@ -26,4 +8,9 @@ export async function load({ cookies}) {
     if (!url || !username || !password) {
         throw redirect(302, '/')
     }
+    let client = await login(url, username, password);
+    let grades = await client.getGradebook();
+    return {
+        grades: JSON.parse(grades)
+    };
 }
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,9 +1,22 @@
 <script>
-	export let form;
+	export let data;
+	let selectedCourse = null;
+	let courseOptions = [{
+    title: "ADV PLACEMENT ERPN HISTORY (SS220H)",
+    id: "math-101"
+}
+];
 </script>
-
-{#if form}
-	{#each form?.grades.Gradebook.Courses.Course as course}
+<label for="courseDropdown">Select a Course:</label>
+    <select id="courseDropdown" bind:value={selectedCourse} on:change={(event) => {selectedCourse = event.target.value}}>
+        <option value="">Choose a course</option>
+        {#each courseOptions as option}
+            <option value={option.id}>{option.title}</option>
+        {/each}
+    </select>
+{#if data}
+	{#each data?.grades.Gradebook.Courses.Course as course}
+	{#if selectedCourse === course.Title || !selectedCourse}
         <h1>{course.Title}</h1>
         <h3>Taught by {course.Staff}</h3>
 		{#each course.Marks.Mark.Assignments.Assignment as assignment}
@@ -11,5 +24,6 @@
             <p>Grade: {assignment.Score}</p>
 			<br />
 		{/each}
+		{/if}
 	{/each}
 {/if}
