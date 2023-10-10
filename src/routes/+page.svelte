254eee8fdb4817059b965f6649fe937303d3dc66
Jax-Hax <jaxhacks543@gmail.com>
2023-10-10 17:00:21 -0400

    added course title and teacher

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,6 +21,8 @@
 </form>
 {#if form}
 	{#each form?.grades.Gradebook.Courses.Course as course}
+        <h1>{course.Title}</h1>
+        <h3>Taught by {course.Staff}</h3>
 		{#each course.Marks.Mark.Assignments.Assignment as assignment}
 			<p>Name: {assignment.Measure}</p>
             <p>Grade: {assignment.Score}</p>
