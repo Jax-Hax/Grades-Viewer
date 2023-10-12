7b99ac708cb76d6513d96228d6017920f20549f6
Jax-Hax <jaxhacks543@gmail.com>
2023-10-12 17:36:41 -0400

    styling but had to fix urgent error with stockshare

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -29,17 +29,41 @@
 		<option value={option}>{option}</option>
 	{/each}
 </select>
+
 {#if data}
+<div id="courseGrid">
 	{#each courses as course}
 		{#if selectedCourse === course.Title || !selectedCourse}
-			<h1>{course.Title}</h1>
-			<h3>Taught by {course.Staff}</h3>
-			<h3>{course.Marks.Mark.CalculatedScoreString}</h3>
-			{#each course.Marks.Mark.Assignments.Assignment as assignment}
-				<p>Name: {assignment.Measure}</p>
-				<p>Grade: {assignment.Score}</p>
-				<br />
-			{/each}
+			<div>
+				<h1>{course.Title}</h1>
+				<h3>Taught by {course.Staff}</h3>
+				<h3>{course.Marks.Mark.CalculatedScoreString}</h3>
+				{#each course.Marks.Mark.Assignments.Assignment as assignment}
+					<p>Name: {assignment.Measure}</p>
+					<p>Grade: {assignment.Score}</p>
+					<br />
+				{/each}
+			</div>
 		{/if}
 	{/each}
+</div>
 {/if}
+
+<style>
+	#courseGrid {
+		display: grid;
+		gap: 1rem;
+		grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+		align-items: center;
+		padding: 1em;
+	}
+	#courseGrid div {
+		background-color: #2c2c2c;
+		padding: 2em;
+		color: white;
+		border-radius: 16px;
+		font-size: 1.25em;
+		border-radius: 16px;
+		text-align: center;
+	}
+</style>
